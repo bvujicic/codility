19da0b3d9192a3b0445d9e143a91771a9651d73e
Boris Vujičić <boris.vujicic@broj42.com>
2017-10-19 13:35:07 +0200

    Fix

--- lesson_1.py
+++ lesson_1.py
@@ -4,7 +4,7 @@ Test 1: BinaryGap
 
 
 def binary_gap(N):
-    binary_string = format(2, 'b')
+    binary_string = format(N, 'b')
     zero_gaps = binary_string.strip('0').split('1')
 
     length_of_max_gap = len(max(zero_gaps))
